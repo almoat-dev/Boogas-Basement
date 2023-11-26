f0c5373c23f88a80591aaf5d5e1bd36de4b97c15
almoat-dev <almoat@umich.edu>
2023-11-26 06:39:21 -0500

    Added Sprint function

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f; 
+    public float speed = 8f;
+    public float sprintSpeed = 20f;
+    public float maxStamina = 100.0f;
+    public float staminaConsumptionRate = 6.0f;
+    public float staminaRecoveryRate = 5.0f;
+
+    private float currentStamina;
 
     // Update is called once per frame
     void Update()
@@ -16,10 +22,24 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        move.Normalize();
+
         controller.Move(move * speed * Time.deltaTime);
 
-        Vector3 clampedPosition = transform.position;
-        clampedPosition.y = Mathf.Clamp(clampedPosition.y, 0,2); // Set minY and maxY according to your needs
+        if (Input.GetKey(KeyCode.LeftShift) && currentStamina > 1)
+        {
+            speed = sprintSpeed;
+            currentStamina -= staminaConsumptionRate * Time.deltaTime;
+        }
+        else
+        {
+            speed = 10f;
+            currentStamina += staminaRecoveryRate * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+        }
+
+            Vector3 clampedPosition = transform.position;
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, 0,2); 
         transform.position = clampedPosition;
     }
 }
